4d83ae299684fb2b35ebc3987054609a813e93fd
mminvestai <123886693+mminvestai@users.noreply.github.com>
2023-03-11 13:32:06 -0300

    Update main.py

--- csvtobq/main.py
+++ csvtobq/main.py
@@ -4,9 +4,10 @@ def csvtobq(request):
   content_type = request.headers['content-type']
   request_json = request.get_json(silent=True)
 
-  if request_json and 'table_id' in request_json and 'uri' in request_json:
+  if request_json and 'table_id' in request_json and 'uri' in request_json and 'replace_flag': #MMP 2023-03-11
       table_id = request_json['table_id']
       uri = request_json['uri']
+      replace_flag = request_json['replace_flag'] #MMP 2023-03-11
   else:
       raise ValueError("JSON is invalid, or missing 'table_id' or 'uri' property")
       
@@ -18,6 +19,16 @@ def csvtobq(request):
   skip_leading_rows=1)
   #table_id = "warren-says.data.predictions"
   #uri = "gs://raw-data_bucket/predict_poc.csv"
+  
+  #+MMP 2023-03-11
+  if replace_flag == 1:
+    job_config = bigquery.LoadJobConfig(
+      write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+      source_format=bigquery.SourceFormat.CSV,
+      skip_leading_rows=1,
+    )
+  #-MMP 2023-03-11
+  
   load_job = client.load_table_from_uri(uri, table_id, job_config=job_config)
 
   load_job.result()  # Waits for the job to complete.
